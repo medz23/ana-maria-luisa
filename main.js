405eb80726c1e504859f44a07b38967c7385d64a
medz23 <72319839+medz23@users.noreply.github.com>
2021-09-27 00:37:18 +0300

    done with work section

--- main.js
+++ main.js
@@ -10,3 +10,123 @@ mainNavLinks.forEach(link => {
     });
   });
 });
+
+var i = 0,
+  a = 0,
+  isBackspacing = false,
+  isParagraph = false;
+
+// Typerwrite text content. Use a pipe to indicate the start of the second line "|".
+var textArray = [
+  "|FULL STACK WEB DEVELOPER",
+  "|ARTIFICIAL INTELLIGENCE STUDENT",
+  "|ANDROID DEVELOPMENT ENTHUSIAST",
+];
+
+
+var speedForward = 100,
+  speedWait = 1000,
+  speedBetweenLines = 1000,
+  speedBackspace = 25;
+
+
+typeWriter("lead", textArray);
+
+function typeWriter(id, ar) {
+  var element = $("#" + id),
+    aString = ar[a],
+    eHeader = element.children("h1"),
+    eParagraph = element.children("p");
+
+  if (!isBackspacing) {
+
+    if (i < aString.length) {
+
+      if (aString.charAt(i) == "|") {
+        isParagraph = true;
+        eHeader.removeClass("font-weight-light");
+        eParagraph.addClass("font-weight-light");
+        i++;
+        setTimeout(function() {
+          typeWriter(id, ar);
+        }, speedBetweenLines);
+
+      } else {
+
+        if (!isParagraph) {
+          eHeader.text(eHeader.text() + aString.charAt(i));
+        } else {
+          eParagraph.text(eParagraph.text() + aString.charAt(i));
+        }
+        i++;
+        setTimeout(function() {
+          typeWriter(id, ar);
+        }, speedForward);
+      }
+
+    } else if (i == aString.length) {
+
+      isBackspacing = true;
+      setTimeout(function() {
+        typeWriter(id, ar);
+      }, speedWait);
+
+    }
+
+  } else {
+
+    if (eHeader.text().length > 0 || eParagraph.text().length > 0) {
+
+      if (eParagraph.text().length > 0) {
+        eParagraph.text(eParagraph.text().substring(0, eParagraph.text().length - 1));
+      } else if (eHeader.text().length > 0) {
+        eParagraph.removeClass("font-weight-light");
+        eHeader.addClass("font-weight-light");
+        eHeader.text(eHeader.text().substring(0, eHeader.text().length - 1));
+      }
+      setTimeout(function() {
+        typeWriter(id, ar);
+      }, speedBackspace);
+
+
+    } else {
+
+      isBackspacing = false;
+      i = 0;
+      isParagraph = false;
+      a = (a + 1) % ar.length;
+      setTimeout(function() {
+        typeWriter(id, ar);
+      }, 50);
+
+    }
+  }
+}
+
+
+function openWork(evt, cityName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+  if (cityName === "Skills" && document.getElementById(cityName).style.display === "block") {
+    $(".skillbar").each(function() {
+      $(this)
+        .find(".skillbar-bar")
+        .animate({
+            width: $(this).attr("data-percent")
+          },
+          2500)
+
+    });
+
+  }
+}
